0b7eda2edac3f96d1124d54a27967f00f08cb3aa
jiindev <jiindev@gmail.com>
2019-10-04 13:55:41 +0900

    랜덤으로 테이블에 지뢰 뿌려주기 구현
    피셔예이츠 셔플 방식으로 주어진 갯수대로 지뢰를 형성하고 이를 화면에 뿌려주는 것을 구현함

---  minesweeper/minesweeper.js
+++  minesweeper/minesweeper.js
@@ -4,19 +4,38 @@ document.querySelector('#exec').addEventListener('click',function(){
     let mine = parseInt(document.querySelector('#mine').value);
     console.log(hor, ver, mine);
 
+    //지뢰의 위치 뽑기
+    let num_list = Array(hor * ver).fill().map(function(item, index){
+        return index;
+    });
+    let shuffle = [];
+    while(num_list.length>hor*ver-mine){
+        let selected_num = num_list.splice(Math.floor(Math.random() * num_list.length),1)[0];
+        shuffle.push(selected_num);
+    }
+    console.log(shuffle);
+
+    //지뢰 테이블 만들기
     let dataset = [];
-    let tbody = document.querySelector('#table tbody');
+    let table = document.querySelector('#table');
     for(let i=0; i<ver; i++){
-        var arr=[];
-        var tr = document.createElement('tr');
+        let arr=[];
+        let tr = document.createElement('tr');
         dataset.push(arr);
         for(let j=0; j<hor; j++){
             arr.push(1);
-            var td = document.createElement('td');
+            let td = document.createElement('td');
             tr.appendChild(td);
-            td.textContent=1;
         }
-        tbody.appendChild(tr);
+        table.appendChild(tr);
     }
     console.log(dataset);
+
+    //지뢰 심기
+    for (let k=0; k<shuffle.length; k++){
+        let ver_pos = Math.floor(shuffle[k]/hor);
+        let hor_pos = shuffle[k]%hor;
+        table.children[ver_pos].children[hor_pos].textContent="X";
+        dataset[ver_pos][hor_pos]="X";
+    }
 });
