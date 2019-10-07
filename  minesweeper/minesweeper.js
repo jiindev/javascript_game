bdb5d5d0576287af4859ccfca2f3dd542c0b88b5
jiindev <jiindev@gmail.com>
2019-10-07 11:05:59 +0900

    우클릭으로 깃발 꼽기
    우클릭 시 해당되는 영역의 화면상에 !를 표시하고 데이터를 !로 변환한다.

---  minesweeper/minesweeper.js
+++  minesweeper/minesweeper.js
@@ -1,3 +1,6 @@
+let table = document.querySelector('#table');
+let dataset = [];
+
 document.querySelector('#exec').addEventListener('click',function(){
     let hor = parseInt(document.querySelector('#hor').value);
     let ver = parseInt(document.querySelector('#ver').value);
@@ -16,8 +19,6 @@ document.querySelector('#exec').addEventListener('click',function(){
     console.log(shuffle);
 
     //지뢰 테이블 만들기
-    let dataset = [];
-    let table = document.querySelector('#table');
     for(let i=0; i<ver; i++){
         let arr=[];
         let tr = document.createElement('tr');
@@ -25,11 +26,22 @@ document.querySelector('#exec').addEventListener('click',function(){
         for(let j=0; j<hor; j++){
             arr.push(1);
             let td = document.createElement('td');
+            td.addEventListener('contextmenu',function(e){
+                e.preventDefault();
+                let parentTr = e.currentTarget.parentNode;
+                let parentTable = e.currentTarget.parentNode.parentNode;
+                let tdX = Array.prototype.indexOf.call(parentTr.children,e.currentTarget);
+                let tdY = Array.prototype.indexOf.call(parentTable.children,parentTr);
+                console.log(parentTr, parentTable, e.currentTarget, tdX, tdY);
+                e.currentTarget.textContent='!';
+                dataset[tdY][tdX]='!';
+                console.log(dataset);
+            });
             tr.appendChild(td);
         }
         table.appendChild(tr);
     }
-    console.log(dataset);
+    
 
     //지뢰 심기
     for (let k=0; k<shuffle.length; k++){
@@ -38,4 +50,7 @@ document.querySelector('#exec').addEventListener('click',function(){
         table.children[ver_pos].children[hor_pos].textContent="X";
         dataset[ver_pos][hor_pos]="X";
     }
+
+    console.log(dataset);
 });
+
